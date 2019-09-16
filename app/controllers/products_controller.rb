7939036b111452748560494e6cb2aecb0365e223
github username <github email address>
2019-09-16 16:30:21 -0600

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,10 @@
 class ProductsController < ApplicationController
-
+    def index
+        @products = Product.all
+    end
+    
+    def description 
+        product = Product.find(params[:id])
+        render plain: product.description
+    end
 end
